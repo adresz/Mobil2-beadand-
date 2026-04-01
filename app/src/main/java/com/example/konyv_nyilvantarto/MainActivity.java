ece4aaac0a8f1f079dd1792e0b3ba2b5bb2f3ea0
adresz <tigerad97@gmail.com>
2026-04-01 12:13:58 +0200

    Added imageview, edit text, star rating
    Added imageview for cover of book
    Added edit text for detailed opinion on book
    Added star rating.

--- app/src/main/java/com/example/konyv_nyilvantarto/MainActivity.java
+++ app/src/main/java/com/example/konyv_nyilvantarto/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.book_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mtbHeader), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clMain), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
