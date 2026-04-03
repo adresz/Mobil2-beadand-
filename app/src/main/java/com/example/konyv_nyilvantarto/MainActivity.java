767dafd7c4ad9241258d0a11a5e446cd1952c249
Nagy Márk <marci.2003.11@gmail.com>
2026-04-03 17:14:27 +0200

    Created conneection for recyclerview

--- app/src/main/java/com/example/konyv_nyilvantarto/MainActivity.java
+++ app/src/main/java/com/example/konyv_nyilvantarto/MainActivity.java
@@ -7,6 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kotlin.collections.ArrayDeque;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +21,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.book_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clMain), (v, insets) -> {
+        setContentView(R.layout.home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.clHome), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        RecyclerView recyclerView = findViewById(R.id.rvMyBooks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List<BookHome> bookList = new ArrayList<>();
+
+        bookList.add(new BookHome("Én, a kétarcú", "Ambrózy Áron György", 2026, 90, R.drawable.ketarcu));
+        bookList.add(new BookHome("Egri Csillagok", "Gárdonyi Géza", 1899, 45, R.drawable.egricsillagok));
+        bookList.add(new BookHome("A Pál utcai fiúk", "Molnár Ferenc", 1906, 100, R.drawable.apalutcaifiuk));
+
+        BookAdapterHome adapter = new BookAdapterHome(bookList);
+        recyclerView.setAdapter(adapter);
     }
 }
