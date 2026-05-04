13c50c6f59b9cb07c8d379274a1951ef65d73880
Tiger Adrián <tigerad97@gmail.com>
2026-05-05 00:24:18 +0200

    Added findview by id-s to detailview and set its texts to the transfered arguments

--- app/src/main/java/com/example/konyv_nyilvantarto/fragments/BookDetailsFragment.java
+++ app/src/main/java/com/example/konyv_nyilvantarto/fragments/BookDetailsFragment.java
@@ -2,48 +2,37 @@ package com.example.konyv_nyilvantarto.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.konyv_nyilvantarto.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BookDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BookDetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private EditText etBookTitleD;
+    private EditText etAuthorD;
+    private EditText etReleaseD;
+    private EditText etPagesD;
+    private EditText etReadPagesD;
+    private EditText etMaxPagesD;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText etGenreD;
+    private ImageView ivBookCoverD;
 
     public BookDetailsFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static BookDetailsFragment newInstance(String param1, String param2) {
         BookDetailsFragment fragment = new BookDetailsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,10 +40,6 @@ public class BookDetailsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +47,27 @@ public class BookDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_book_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        etBookTitleD = view.findViewById(R.id.etBookTitleD);
+        etAuthorD = view.findViewById(R.id.etAuthorD);
+        etReleaseD = view.findViewById(R.id.etReleaseD);
+        etPagesD = view.findViewById(R.id.etPagesD);
+        etReadPagesD = view.findViewById(R.id.etReadPagesD);
+        etMaxPagesD = view.findViewById(R.id.etMaxPagesD);
+        etGenreD = view.findViewById(R.id.etGenreD);
+
+        ivBookCoverD = view.findViewById(R.id.ivBookCoverD);
+
+        etBookTitleD.setText("Cím: " + getArguments().getString("title"));
+        etAuthorD.setText(getArguments().getString("author"));
+        etReleaseD.setText(getArguments().getString("year"));
+        etGenreD.setText(getArguments().getString("genre"));
+        etPagesD.setText(String.valueOf(getArguments().getInt("maxPages")));
     }
 }
