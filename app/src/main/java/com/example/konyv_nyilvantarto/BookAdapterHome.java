8ff5668dd4a248267cf01fad592f3978cee53f11
Nagy Márk <marci.2003.11@gmail.com>
2026-05-12 21:23:38 +0200

    Updated Adapter with the percentage and delete button

--- app/src/main/java/com/example/konyv_nyilvantarto/BookAdapterHome.java
+++ app/src/main/java/com/example/konyv_nyilvantarto/BookAdapterHome.java
@@ -3,6 +3,7 @@ package com.example.konyv_nyilvantarto;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ public class BookAdapterHome extends RecyclerView.Adapter<BookAdapterHome.BookVi
 
     public interface OnBookClickListener{
         void onBookClick(BookItemHome book);
+        void onDeleteClick(BookItemHome book, int position);
     }
 
     public BookAdapterHome(List<BookItemHome> bookList, OnBookClickListener listener) {
@@ -53,8 +55,18 @@ public class BookAdapterHome extends RecyclerView.Adapter<BookAdapterHome.BookVi
         }
 
         // ProgressBar beállítása
-        holder.pbProgress.setMax(book.getMax_pages());
-        holder.pbProgress.setProgress(book.getCurrent_page());
+        int maxpages = book.getMax_pages();
+        int currentPage = book.getCurrent_page();
+
+        holder.pbProgress.setMax(maxpages);
+        holder.pbProgress.setProgress(currentPage);
+
+        if(maxpages > 0) {
+            int percentage = (int) ((currentPage * 100.0f) / maxpages);
+            holder.tvPercentage.setText(percentage + "%");
+        } else {
+            holder.tvPercentage.setText("0%");
+        }
 
         Glide.with(holder.itemView.getContext())
                 .load(book.getCover())
@@ -63,6 +75,11 @@ public class BookAdapterHome extends RecyclerView.Adapter<BookAdapterHome.BookVi
         holder.itemView.setOnClickListener(v -> {
             listener.onBookClick(book);
         });
+
+        holder.btnDelete.setOnClickListener(v -> {
+            listener.onDeleteClick(book, position);
+        });
+
     }
 
     @Override
@@ -71,9 +88,10 @@ public class BookAdapterHome extends RecyclerView.Adapter<BookAdapterHome.BookVi
     }
 
     public static class BookViewHolder extends RecyclerView.ViewHolder {
-        TextView tvTitle, tvAuthor, tvYear;
+        TextView tvTitle, tvAuthor, tvYear, tvPercentage;
         ProgressBar pbProgress;
         ImageView ivBookCover;
+        ImageButton btnDelete;
 
         public BookViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -82,6 +100,8 @@ public class BookAdapterHome extends RecyclerView.Adapter<BookAdapterHome.BookVi
             tvYear = itemView.findViewById(R.id.tvYear);
             pbProgress = itemView.findViewById(R.id.pbProgress);
             ivBookCover = itemView.findViewById(R.id.ivBookCover);
+            tvPercentage = itemView.findViewById(R.id.tv_Percentage);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
         }
     }
 }
