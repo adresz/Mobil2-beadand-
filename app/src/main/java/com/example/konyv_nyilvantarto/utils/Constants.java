0f5dfa165d392f5faa8736577865efc63858bf9e
Nagy Márk <marci.2003.11@gmail.com>
2026-05-12 22:15:06 +0200

    Added a class for supabase url and api key

---
+++ app/src/main/java/com/example/konyv_nyilvantarto/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.example.konyv_nyilvantarto.utils;
+
+public class Constants {
+    public static final String SUPABASE_BASE_URL = "https://plohvgiccntmsgzyszrl.supabase.co/rest/v1/";
+    public static final String SUPABASE_API_KEY = "sb_publishable_cuiKHkTKKdAHSnMkBcwghQ_rKE7skpG";
+}
