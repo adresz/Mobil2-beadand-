9a32c4f3657c94871620e701808a74a57578044e
Tiger Adrián <tigerad97@gmail.com>
2026-05-18 22:33:47 +0200

    Added possibility for image transfer towards bucket

--- app/src/main/java/com/example/konyv_nyilvantarto/BookApi.java
+++ app/src/main/java/com/example/konyv_nyilvantarto/BookApi.java
@@ -1,11 +1,16 @@
 package com.example.konyv_nyilvantarto;
 import com.example.konyv_nyilvantarto.fragments.FavoriteBook;
 
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface BookApi {
@@ -17,5 +22,14 @@ public interface BookApi {
             @Header("apikey") String apiKey,         // MUST be all lowercase
             @Header("Authorization") String auth,    // Standard capitalization
             @Body FavoriteBook book
+    );
+
+    @Multipart
+    @POST("storage/v1/object/book_covers/{profile_path}")
+    Call<ResponseBody> uploadBookCover(
+            @Header("apikey") String apiKey,
+            @Header("Authorization") String auth,
+            @Path("profile_path") String fileName,
+            @Part MultipartBody.Part file
     );
 }
